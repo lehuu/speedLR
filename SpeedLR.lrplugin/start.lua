8ad8355b8b1e5f48b49d8d056eed19c2701cbc86
Phuoc Le <phuoc.le92@gmail.com>
2024-10-21 00:55:21 +0900

    add full develop params and detailed bezel

--- SpeedLR.lrplugin/start.lua
+++ SpeedLR.lrplugin/start.lua
@@ -9,71 +9,170 @@ local LrSocket = import "LrSocket"
 local LrTableUtils = import "LrTableUtils"
 local LrPathUtils = import "LrPathUtils"
 local LrFileUtils = import "LrFileUtils"
---==============================================================================
+-- ==============================================================================
 -- Port numbers
 -- port zero indicates that we want the OS to auto-assign the port
 local AUTO_PORT = 0
 -- port number used to receive commands
 local defaultReceivePort = 49000
---==============================================================================
+-- ==============================================================================
 
 -- All of the Develop parameters that we will monitor for changes. For a complete
 -- listing of all parameter names, see the API documentation for LrDevelopController.
-local develop_params = {
-	"Temperature",
-	"Tint",
-	"Exposure",
-	"Contrast",
-	"Highlights",
-	"Shadows",
-	"Whites",
-	"Blacks",
-	"Clarity",
-	"Vibrance",
-	"Saturation",
-	}
+local develop_params = {"Temperature", "Tint", "Exposure", "Contrast", "Highlights", "Shadows", "Whites", "Blacks",
+                        "Clarity", "Vibrance", "Saturation", "PresetAmount", "ProfileAmount", -- Tone Panel
+"ParametricDarks", "ParametricLights", "ParametricShadows", "ParametricHighlights", "ParametricShadowSplit",
+                        "ParametricMidtoneSplit", "ParametricHighlightSplit", -- HSL / Color
+"SaturationAdjustmentRed", "SaturationAdjustmentOrange", "SaturationAdjustmentYellow", "SaturationAdjustmentGreen",
+                        "SaturationAdjustmentAqua", "SaturationAdjustmentBlue", "SaturationAdjustmentPurple",
+                        "SaturationAdjustmentMagenta", "HueAdjustmentRed", "HueAdjustmentOrange", "HueAdjustmentYellow",
+                        "HueAdjustmentGreen", "HueAdjustmentAqua", "HueAdjustmentBlue", "HueAdjustmentPurple",
+                        "HueAdjustmentMagenta", "LuminanceAdjustmentRed", "LuminanceAdjustmentOrange",
+                        "LuminanceAdjustmentYellow", "LuminanceAdjustmentGreen", "LuminanceAdjustmentAqua",
+                        "LuminanceAdjustmentBlue", "LuminanceAdjustmentPurple", "LuminanceAdjustmentMagenta",
+                        "PointColors", -- B & W
+"GrayMixerRed", "GrayMixerOrange", "GrayMixerYellow", "GrayMixerGreen", "GrayMixerAqua", "GrayMixerBlue",
+                        "GrayMixerPurple", "GrayMixerMagenta", -- Split Toning
+"SplitToningShadowHue", "SplitToningShadowSaturation", "ColorGradeShadowLum", "SplitToningHighlightHue",
+                        "SplitToningHighlightSaturation", "ColorGradeHighlightLum", "ColorGradeMidtoneHue",
+                        "ColorGradeMidtoneSat", "ColorGradeMidtoneLum", "ColorGradeGlobalHue", "ColorGradeGlobalSat",
+                        "ColorGradeGlobalLum", "SplitToningBalance", "ColorGradeBlending", -- detail Panel
+"Sharpness", "SharpenRadius", "SharpenDetail", "SharpenEdgeMasking", "LuminanceSmoothing",
+                        "LuminanceNoiseReductionDetail", "LuminanceNoiseReductionContrast", "ColorNoiseReduction",
+                        "ColorNoiseReductionDetail", "ColorNoiseReductionSmoothness", -- Dehaze
+"Dehaze", -- Post-Crop Vignetting
+"PostCropVignetteAmount", "PostCropVignetteMidpoint", "PostCropVignetteFeather", "PostCropVignetteRoundness",
+                        "PostCropVignetteStyle", "PostCropVignetteHighlightContrast", -- Grain
+"GrainAmount", "GrainSize", "GrainFrequency", -- Profile
+"LensProfileDistortionScale", "LensProfileChromaticAberrationScale", "LensProfileVignettingScale",
+                        "LensManualDistortionAmount", -- Color
+"DefringePurpleAmount", "DefringePurpleHueLo", "DefringePurpleHueHi", "DefringeGreenAmount", "DefringeGreenHueLo",
+                        "DefringeGreenHueHi", -- Manual Perspective
+"PerspectiveVertical", "PerspectiveHorizontal", "PerspectiveRotate", "PerspectiveScale", "PerspectiveAspect",
+                        "PerspectiveX", "PerspectiveY", "PerspectiveUpright", -- Calibrate Panel
+"ShadowTint", "RedHue", "RedSaturation", "GreenHue", "GreenSaturation", "BlueHue", "BlueSaturation", -- Cropping
+"straightenAngle", -- Local Adjustments
+"local_Temperature", "local_Tint", "local_Exposure", "local_Contrast", "local_Highlights", "local_Shadows",
+                        "local_Clarity", "local_Saturation", "local_ToningHue", "local_ToningSaturation",
+                        "local_Sharpness", "local_LuminanceNoise", "local_Moire", "local_Defringe", "local_Blacks",
+                        "local_Whites", "local_Dehaze", "local_PointColors", "local_Texture", "local_Hue",
+                        "local_Amount", "local_Maincurve", "local_Redcurve", "local_Greencurve", "local_Bluecurve",
+                        "local_Grain", "local_RefineSaturation"}
+
 local develop_param_set = {}
-for _, key in ipairs( develop_params ) do
-	develop_param_set[ key ] = true
+for _, key in ipairs(develop_params) do
+    develop_param_set[key] = true
+end
+
+local currentSetting = {
+    key = nil,
+    value = nil
+}
+
+local last_debounce_call = 0
+local delay = 1.5
+
+local function debounce(key, value)
+    local now = os.clock()
+
+    if now - last_debounce_call >= delay then
+        currentSetting.key = nil
+        currentSetting.value = nil
+        last_debounce_call = now -- Update the last call time
+    else
+        last_debounce_call = now
+    end
+
+    if currentSetting.key == nil or currentSetting.key ~= key then
+        currentSetting.key = key
+        currentSetting.value = value
+    end
 end
 
 --------------------------------------------------------------------------------
 -- Given a key/value pair that has been parsed from a receiver port message, calls
 -- the appropriate API to adjust a setting in Lr.
-local function setValue( key, value )
-	modName = LrApplicationView.getCurrentModuleName()
-	if( modName ~= "develop") then
-		return
-	end	
-	if value == "+" then -- ex: "Exposure = +"
-		LrDevelopController.increment( key )
-		return true
-	end
-	if value == "-" then -- ex: "Exposure = -"
-		LrDevelopController.decrement( key )
-		return true
-	end
-	if value == "reset" then -- ex: "Exposure = reset"
-		LrDevelopController.resetToDefault( key )
-		return true
-	end
-	local numericValue = tonumber( value )
-	if numericValue then
-		if key and develop_param_set[ key ] then -- ex: "Exposure = 1.5"
-			LrDevelopController.setValue( key, numericValue )
-			return true
-		end
-	end
+local function setValue(key, value)
+    local modName = LrApplicationView.getCurrentModuleName()
+    if (modName ~= "develop") then
+        return
+    end
+    if value == "reset" then -- ex: "Exposure = reset"
+        LrDevelopController.resetToDefault(key)
+		LrDialogs.showBezel(key .. " reset", delay)
+        return true
+    end
+    if value:sub(-1) == "%" then
+        local numericValue = tonumber(value:sub(1, -2))
+        if numericValue then
+            if key and develop_param_set[key] then -- ex: "Exposure = 1.5"
+                local currentValue = LrDevelopController.getValue(key)
+
+                debounce(key, currentValue)
+
+                local min, max = LrDevelopController.getRange(key)
+                local adjusted_max = 100
+
+                if max > 10000 then
+                    adjusted_max = max / 10
+                elseif max > 1000 then
+                    adjusted_max = max / 5
+                elseif max > 500 then
+                    adjusted_max = max / 2
+                elseif max < 100 then
+                    adjusted_max = max
+                end
+
+                local increment = (numericValue * adjusted_max) / 100
+                local newVal = currentValue + increment;
+
+                local prievousValue = currentSetting.value
+                local delta = "0"
+
+                if prievousValue then
+                    delta = newVal - prievousValue
+                    if delta > 0 then
+                        delta = "+" .. delta
+                    end
+                end
+
+                LrDevelopController.setValue(key, newVal)
+                LrDialogs.showBezel(key .. " " .. newVal .. " (" .. delta .. ")", delay)
+                return true
+            end
+        end
+    end
+    local numericValue = tonumber(value)
+    if numericValue then
+        if key and develop_param_set[key] then -- ex: "Exposure = 1.5"
+            local currentValue = LrDevelopController.getValue(key)
+            local newVal = currentValue + numericValue;
+            debounce(key, currentValue)
+
+            local prievousValue = currentSetting.value
+            local delta = "0"
+
+            if prievousValue then
+                delta = newVal - prievousValue
+                if delta > 0 then
+                    delta = "+" .. delta
+                end
+            end
+            LrDevelopController.setValue(key, newVal)
+            LrDialogs.showBezel(key .. " " .. newVal .. " (" .. delta .. ")", delay)
+            return true
+        end
+    end
 end
 
 --------------------------------------------------------------------------------
 -- Simple parser for messages sent from the external process over the socket
 -- connection, formatted as "key = value". (ex: "rating = 2")
-local function parseMessage( data )
-	if type( data ) == "string" then
-		local _, _, key, value = string.find( data, "([^ ]+)%s*=%s*(.*)" )
-		return key, value
-	end
+local function parseMessage(data)
+    if type(data) == "string" then
+        local _, _, key, value = string.find(data, "([^ ]+)%s*=%s*(.*)")
+        return key, value
+    end
 end
 
 --------------------------------------------------------------------------------
@@ -83,89 +182,89 @@ local makingReceiver = false
 --------------------------------------------------------------------------------
 
 local function getPortFromFile()
-	local thePort = defaultReceivePort
-	local path  = LrPathUtils.getStandardFilePath( "appData" )
-	path = path.."\\..\\..\\Ebo\\StreamDeckLightroom"
-	path = LrPathUtils.standardizePath( path )
-	LrFileUtils.createAllDirectories( path )
-	path = path.."\\port.config"
-	if LrFileUtils.exists( path ) then
-		thePort = LrFileUtils.readFile( path )
-	end
-	return thePort
+    local thePort = defaultReceivePort
+    local path = LrPathUtils.getStandardFilePath("appData")
+    path = path .. "\\..\\..\\Ebo\\StreamDeckLightroom"
+    path = LrPathUtils.standardizePath(path)
+    LrFileUtils.createAllDirectories(path)
+    path = path .. "\\port.config"
+    if LrFileUtils.exists(path) then
+        thePort = LrFileUtils.readFile(path)
+    end
+    return thePort
 end
 
 --------------------------------------------------------------------------------
-local function makeReceiverSocket( context )
-	local thePort = getPortFromFile()
-	makingReceiver = true
-	LrDialogs.showBezel("Opening streamdeck at port "..thePort, 1)
-	local receiver = LrSocket.bind {
-	functionContext = context,
-	port = thePort,
-	mode = "receive",
-	plugin = _PLUGIN,
-	onConnecting = function( socket, port )
-					receiverPort = port
-				end,
-	onConnected = function( socket, port )
-					receiverConnected = true
-					makingReceiver = false
-				end,
-	onClosed = function( socket )
-					-- If the other side of this socket is closed,
-					-- tell the run loop below that it should exit.
-					--_G.running = false
-					receiverConnected = false
-					makingReceiver = false
-				end,
-	onMessage = function( socket, message )
-					if type( message ) == "string" then
-						local key, value = parseMessage( message )
-						if key and value then
-							if setValue( key, value ) then
-								-- LrDialogs.showBezel( string.format( "%s %s!!!", tostring( key ), tostring( value ) ), 4 )
-							else
-								LrDialogs.showBezel("Unknown command: \""..message.."\"", 1 )
-							end
-						else
-							LrDialogs.showBezel("Unknown command: \""..message.."\"", 1 )
-						end
-					end
-				end,
-	onError = function( socket, err )
-					if err == "timeout" then
-						socket:reconnect()
-					end
-				end,
-	}
-	-- automatically scroll sliders into view whenever they are adjusted
-	LrDevelopController.revealAdjustedControls( true )
-	return receiver
+local function makeReceiverSocket(context)
+    local thePort = getPortFromFile()
+    makingReceiver = true
+    LrDialogs.showBezel("Opening speedLR at port " .. thePort, 1)
+    local receiver = LrSocket.bind {
+        functionContext = context,
+        port = thePort,
+        mode = "receive",
+        plugin = _PLUGIN,
+        onConnecting = function(socket, port)
+            receiverPort = port
+        end,
+        onConnected = function(socket, port)
+            receiverConnected = true
+            makingReceiver = false
+        end,
+        onClosed = function(socket)
+            -- If the other side of this socket is closed,
+            -- tell the run loop below that it should exit.
+            -- _G.running = false
+            receiverConnected = false
+            makingReceiver = false
+        end,
+        onMessage = function(socket, message)
+            if type(message) == "string" then
+                local key, value = parseMessage(message)
+                if key and value then
+                    if setValue(key, value) then
+                        -- LrDialogs.showBezel( string.format( "%s %s!!!", tostring( key ), tostring( value ) ), 4 )
+                    else
+                        LrDialogs.showBezel("Unknown command: \"" .. message .. "\"", 1)
+                    end
+                else
+                    LrDialogs.showBezel("Unknown command: \"" .. message .. "\"", 1)
+                end
+            end
+        end,
+        onError = function(socket, err)
+            if err == "timeout" then
+                socket:reconnect()
+            end
+        end
+    }
+    -- automatically scroll sliders into view whenever they are adjusted
+    LrDevelopController.revealAdjustedControls(true)
+    return receiver
 end
 
 --------------------------------------------------------------------------------
 -- Start everything in an async task so we can sleep in a loop until we are shut down.
-LrTasks.startAsyncTask( function()
-	-- A function context is required for the socket API below. When this context
-	-- is exited all socket connections that have been created from it will be
-	-- closed. We stay inside this context indefinitiely by spinning in a sleep
-	-- loop until told to exit.
-	LrFunctionContext.callWithContext( 'socket_remote', function( context )
-		-- Loop until this plug-in global is set to false, which happens when the external process
-		-- closes the socket connection(s), or if the user selects the menu command "File >
-		-- Plug-in Extras > Stop" , or when Lightroom is shutting down.
-		_G.running = true
-		while _G.running do
-			if receiverConnected == false and makingReceiver == false then
-				makeReceiverSocket(context)
-			end
-			LrTasks.sleep( 1/2 ) -- seconds
-		end
-		_G.shutdown = true
-		if receiverConnected then
-			receiver:close()
-		end
-		LrDialogs.showBezel( "Remote connection closed", 4 )
-		end )
-end )
+LrTasks.startAsyncTask(function()
+    -- A function context is required for the socket API below. When this context
+    -- is exited all socket connections that have been created from it will be
+    -- closed. We stay inside this context indefinitiely by spinning in a sleep
+    -- loop until told to exit.
+    LrFunctionContext.callWithContext('socket_remote', function(context)
+        -- Loop until this plug-in global is set to false, which happens when the external process
+        -- closes the socket connection(s), or if the user selects the menu command "File >
+        -- Plug-in Extras > Stop" , or when Lightroom is shutting down.
+        _G.running = true
+        while _G.running do
+            if receiverConnected == false and makingReceiver == false then
+                makeReceiverSocket(context)
+            end
+            LrTasks.sleep(1 / 2) -- seconds
+        end
+        _G.shutdown = true
+        if receiverConnected then
+            receiver:close()
+        end
+        LrDialogs.showBezel("Remote connection closed", 4)
+    end)
+end)
