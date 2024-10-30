b26611459e476a0813af53b60bd0434ad91461e7
Phuoc Le <phuoc.le92@gmail.com>
2024-10-30 23:16:21 +0900

    add basic controlbutton

---
+++ ControlButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using Button = System.Windows.Controls.Button;
+
+namespace SpeedLR
+{
+    public class ControlButton : Button
+    {
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register(
+                nameof(IsActive),
+                typeof(bool),
+                typeof(ControlButton),
+                new PropertyMetadata(false, OnIsActiveChanged));
+
+        public static readonly DependencyProperty LRCommandProperty = 
+            DependencyProperty.Register(
+                nameof(LRCommand),
+                typeof(string),
+                typeof(ControlButton),
+                new PropertyMetadata(""));
+
+        public bool IsActive
+        {
+            get => (bool)base.GetValue(IsActiveProperty);
+            set => SetValue(IsActiveProperty, value);
+        }
+        public string LRCommand
+        {
+            get => (string)base.GetValue(LRCommandProperty);
+            set => SetValue(LRCommandProperty, value);
+        }
+
+        private static void OnIsActiveChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ControlButton button)
+            {
+                button.Background = (bool)e.NewValue
+                    ? System.Windows.Media.Brushes.Green
+                    : System.Windows.Media.Brushes.Gray;
+            }
+        }
+    }
+}
--- ControllerWindow.xaml.cs
+++ ControllerWindow.xaml.cs
@@ -1,20 +1,81 @@
 ﻿using System.Windows;
+using Button = System.Windows.Controls.Button;
 
 namespace SpeedLR
 {
-    /// <summary>
-    /// Interaction logic for ControllerWindow.xaml
-    /// </summary>
+
     public partial class ControllerWindow : Window
     {
+        private ControlButton[][] menus;
+        private GlobalHotkey escapeHotkey;
+        private GlobalHotkey nextControlHotkey;
+        private GlobalHotkey prevControlHotkey;
+        private GlobalHotkey increaseHotkey;
+        private GlobalHotkey decreaseHotkey;
+
+        private string currentCommand = "";
+
         public ControllerWindow()
         {
             InitializeComponent();
+
+            var firstMenu = new ControlButton[3];
+            firstMenu[0] = this.exposure;
+            firstMenu[1] = this.shadows;
+            firstMenu[2] = this.highlights;
+
+            var secondMenu = new ControlButton[3];
+            secondMenu[0] = this.blacks;
+            secondMenu[1] = this.whites;
+            secondMenu[2] = this.texture;
+
+            menus = new ControlButton[2][];
+            menus[0] = firstMenu;
+            menus[1] = secondMenu;
         }
+
+        private void NavigateControls()
+        {
+
+        }
+
         private void ControllerWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             e.Cancel = true;
             Hide();
+        }
+
+        private void ToggleButton(ControlButton button)
+        {
+            foreach (var submenus in menus)
+            {
+                foreach (var item in submenus)
+                {
+                    if(button != null && item.Name == button.Name)
+                    {
+                        item.IsActive = !item.IsActive;
+                        currentCommand = item.IsActive ? item.LRCommand : "";
+                        continue;
+                    }
+
+                    item.IsActive = false;
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is ControlButton clickedButton)
+            {
+                if (String.IsNullOrEmpty(clickedButton.LRCommand))
+                {
+                    return;
+                }
+
+                ToggleButton(clickedButton);
+
+                //Connector.Instance.SendCommandAsync(clickedButton.LRCommand + "=1%");
+            }
         }
     }
 }
