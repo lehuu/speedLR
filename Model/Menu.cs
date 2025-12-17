215bae4fdbc0dd1a7681eae58efdf797e0d3a821
lehuu <phuoc.le92@gmail.com>
2025-12-17 13:01:24 +0900

    add menu model

---
+++ Model/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpeedLR.Model
+{
+	public class Menu
+	{
+		public Menu(string name)
+		{
+			Name = name;
+			Id = Guid.NewGuid().ToString();
+			Position = 0;
+		}
+
+		[JsonPropertyName("id")]
+		public string Id { get; set; }
+
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("position")]
+		public int Position { get; set; }
+
+	}
+}
---
+++ Model/Submenu.cs
@@ -0,0 +1,11 @@
+﻿namespace SpeedLR.Model
+{
+	public class Submenu : Menu
+	{
+		public Submenu(string name) : base(name)
+		{
+
+		}
+
+	}
+}
