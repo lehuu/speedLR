66aa1fdc7533856287652e231d9c5d84315a7752
lehuu <phuoc.le92@gmail.com>
2025-12-18 18:10:45 +0900

    add dynamic button visibility

---
+++ Utils/ObjectToBoolConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using Binding = System.Windows.Data.Binding;
+
+namespace SpeedLR.Utils
+{
+	public class ObjectToBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType,
+							  object parameter, CultureInfo culture)
+			=> value != null ? true : false;
+
+		public object ConvertBack(object value, Type targetType,
+								  object parameter, CultureInfo culture)
+			=> Binding.DoNothing;
+	}
+}
---
+++ Utils/ObjectToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Binding = System.Windows.Data.Binding;
+
+namespace SpeedLR.Utils
+{
+	public class ObjectToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType,
+							  object parameter, CultureInfo culture)
+			=> value != null ? Visibility.Visible : Visibility.Collapsed;
+
+		public object ConvertBack(object value, Type targetType,
+								  object parameter, CultureInfo culture)
+			=> Binding.DoNothing;
+	}
+}
