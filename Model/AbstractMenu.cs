68fadec76c93eab81dd4bd2c07ed62927dd23ba8
lehuu <phuoc.le92@gmail.com>
2025-12-18 10:21:52 +0900

    add abstractmenu

---
+++ Model/AbstractMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace SpeedLR.Model
+{
+	public abstract class AbstractMenu: INotifyPropertyChanged
+	{
+		protected string _name;
+		protected int _position;
+
+		public AbstractMenu(string name, int position)
+		{
+			_name = name;
+			Id = Guid.NewGuid().ToString();
+			_position = position;
+		}
+
+		[JsonPropertyName("id")]
+		public string Id { get; set; }
+
+		[JsonPropertyName("name")]
+		public string Name
+		{
+			get => _name;
+			set
+			{
+				if (_name != value)
+				{
+					_name = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		[JsonPropertyName("position")]
+		public int Position
+		{
+			get => _position;
+			set
+			{
+				if (_position != value)
+				{
+					_position = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+	}
+}
--- Model/Menu.cs
+++ Model/Menu.cs
@@ -1,57 +1,7 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Text.Json.Serialization;
-
-namespace SpeedLR.Model
+﻿namespace SpeedLR.Model
 {
-	public class Menu: INotifyPropertyChanged
+	public class Menu: AbstractMenu
 	{
-		private string _name;
-		private int _position;
-
-		public Menu(string name, int position)
-		{
-			Name = name;
-			Id = Guid.NewGuid().ToString();
-			Position = position;
-		}
-
-		[JsonPropertyName("id")]
-		public string Id { get; set; }
-
-		[JsonPropertyName("name")]
-		public string Name
-		{
-			get => _name;
-			set
-			{
-				if (_name != value)
-				{
-					_name = value;
-					OnPropertyChanged();
-				}
-			}
-		}
-
-		[JsonPropertyName("position")]
-		public int Position
-		{
-			get => _position;
-			set
-			{
-				if (_position != value)
-				{
-					_position = value;
-					OnPropertyChanged();
-				}
-			}
-		}
-
-		public event PropertyChangedEventHandler PropertyChanged;
-		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-
+		public Menu(string name, int position): base(name, position) { }
 	}
 }
