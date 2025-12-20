633094d9f459e30a4ad8bb8c48a4a23d61f8c88c
lehuu <phuoc.le92@gmail.com>
2025-12-20 21:30:02 +0900

    restructure viewmodel

---
+++ Controls/BaseViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SpeedLR.Model;
+
+namespace SpeedLR.Controls
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		private Menu? _selectedMenu;
+		private Submenu? _selectedSubmenu;
+		private ObservableCollection<Menu> _userMenus = new ObservableCollection<Menu>();
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		public BaseViewModel()
+		{
+			UserMenus = new ObservableCollection<Menu>(LocalData.Instance.UserMenus);
+			SelectedMenu = UserMenus.FirstOrDefault();
+		}
+
+		public Submenu? SelectedSubmenu
+		{
+			get => _selectedSubmenu;
+			set
+			{
+				if (_selectedSubmenu != value)
+				{
+					_selectedSubmenu = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public Menu? SelectedMenu
+		{
+			get => _selectedMenu;
+			set
+			{
+				if (_selectedMenu != value)
+				{
+					_selectedMenu = value;
+					SelectedSubmenu = value?.Submenus.FirstOrDefault();
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public ObservableCollection<Menu> UserMenus
+		{
+			get => _userMenus;
+			set
+			{
+				if (_userMenus != value)
+				{
+					_userMenus = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+	}
+}
--- Controls/MainViewModel.cs
+++ Controls/MainViewModel.cs
@@ -1,62 +1,10 @@
 ﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using SpeedLR.Model;
 
 namespace SpeedLR.Controls
 {
-	public class MainViewModel : INotifyPropertyChanged
+	public class MainViewModel : BaseViewModel
 	{
-		private Menu? _selectedMenu;
-		private Submenu? _selectedSubmenu;
-		private ObservableCollection<Menu> _userMenus = new ObservableCollection<Menu>();
-
-		public MainViewModel()
-		{
-			UserMenus = new ObservableCollection<Menu>(LocalData.Instance.UserMenus);
-			SelectedMenu = UserMenus.FirstOrDefault();
-		}
-
-		public Submenu? SelectedSubmenu
-		{
-			get => _selectedSubmenu;
-			set
-			{
-				if (_selectedSubmenu != value)
-				{
-					_selectedSubmenu = value;
-					OnPropertyChanged();
-				}
-			}
-		}
-
-		public Menu? SelectedMenu
-		{
-			get => _selectedMenu;
-			set
-			{
-				if (_selectedMenu != value)
-				{
-					_selectedMenu = value;
-					SelectedSubmenu = value?.Submenus.FirstOrDefault();
-					OnPropertyChanged();
-				}
-			}
-		}
-
-		public ObservableCollection<Menu> UserMenus
-		{
-			get => _userMenus;
-			set
-			{
-				if (_userMenus != value)
-				{
-					_userMenus = value;
-					OnPropertyChanged();
-				}
-			}
-		}
-
 		public void SaveMenus()
 		{
 			UserMenus = new ObservableCollection<Menu>(UserMenus.OrderBy(m => m.Position));
@@ -150,10 +98,6 @@ namespace SpeedLR.Controls
 			SaveMenus();
 		}
 
-		public event PropertyChangedEventHandler? PropertyChanged;
-		protected void OnPropertyChanged([CallerMemberName] string name = null)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-		}
+
 	}
 }
