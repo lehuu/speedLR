9df9f7fcdce7c64d50e5c0266301edf2e3fe2cdd
lehuu <phuoc.le92@gmail.com>
2025-12-23 19:38:18 +0900

    add fade animation

--- Controls/ControllerWindow.xaml.cs
+++ Controls/ControllerWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Timers;
+﻿using System;
+using System.Linq;
+using System.Timers;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Animation; // Added for animations
 using SpeedLR.Model;
 using SpeedLR.Utils;
 using static SpeedLR.Controls.ControllerViewModel;
@@ -9,13 +12,11 @@ using Timer = System.Timers.Timer;
 
 namespace SpeedLR.Controls
 {
-	/// <summary>
-	/// Interaction logic for ControllerWindow.xaml
-	/// </summary>
 	public partial class ControllerWindow : Window
 	{
 		public ControllerViewModel ViewModel => (ControllerViewModel)DataContext;
-        private readonly Timer _hideTimer;
+		private readonly Timer _hideTimer;
+
 		public ControllerWindow()
 		{
 			InitializeComponent();
@@ -24,6 +25,57 @@ namespace SpeedLR.Controls
 			_hideTimer.Elapsed += OnHideElapsed;
 		}
 
+		// ... [ToggleVisibility and Positioning methods remain unchanged] ...
+
+		private void Command_Scroll(int direction)
+		{
+			if (!ViewModel.IsConnected || ViewModel.SelectedAction == null)
+			{
+				return;
+			}
+
+			// Animate to 0.01 opacity instantly (or very fast)
+			DoubleAnimation fadeOut = new DoubleAnimation
+			{
+				To = 0.01,
+				Duration = TimeSpan.FromMilliseconds(150),
+				EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+			};
+			this.BeginAnimation(OpacityProperty, fadeOut);
+
+			string[] steps = new string[] { "1%", "2%", "5%" };
+			var stepPercentage = steps[(int)ViewModel.StepSize];
+
+			if (direction > 0)
+			{
+				Connector.Instance.SendCommandAsync(ViewModel.SelectedAction.Command + "=+" + stepPercentage);
+			}
+			else
+			{
+				Connector.Instance.SendCommandAsync(ViewModel.SelectedAction.Command + "=-" + stepPercentage);
+			}
+
+			_hideTimer.Stop();
+			_hideTimer.Start();
+		}
+
+		private void OnHideElapsed(object sender, ElapsedEventArgs e)
+		{
+			Dispatcher.Invoke(() =>
+			{
+				// Smoothly animate back to full opacity
+				DoubleAnimation fadeIn = new DoubleAnimation
+				{
+					To = 1.0,
+					Duration = TimeSpan.FromMilliseconds(150),
+					EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseIn }
+				};
+				this.BeginAnimation(OpacityProperty, fadeIn);
+			});
+		}
+
+		// ... [Rest of your helper methods (Command_UpDown, Reset, etc.)] ...
+
 		public void ToggleVisibility(bool isLightroomActive)
 		{
 			if (this.IsVisible)
@@ -31,15 +83,12 @@ namespace SpeedLR.Controls
 				this.Hide();
 				return;
 			}
-
 			if (!isLightroomActive) return;
-
 			if (ViewModel.IsPinned)
 			{
 				this.Show();
 				return;
 			}
-
 			PositionAtMouse();
 			this.Show();
 		}
@@ -55,7 +104,7 @@ namespace SpeedLR.Controls
 		private void PositionAtMouse()
 		{
 			Point mousePosition = System.Windows.Forms.Control.MousePosition;
-			Screen screen = Screen.FromPoint(mousePosition);
+			System.Windows.Forms.Screen screen = System.Windows.Forms.Screen.FromPoint(mousePosition);
 			var scale = DpiHelper.GetDpiScaleFactorForMousePosition();
 
 			var formWidth = Width;
@@ -80,187 +129,71 @@ namespace SpeedLR.Controls
 			Left = x;
 			Top = y;
 		}
-		private void HideButton_Click(object sender, RoutedEventArgs e)
-		{
-			Hide();
-		}
+
+		private void HideButton_Click(object sender, RoutedEventArgs e) => Hide();
+
 		private void DragButton_MouseDown(object sender, MouseButtonEventArgs e)
 		{
-			if (e.ChangedButton == MouseButton.Left)
-			{
-				this.DragMove();
-			}
+			if (e.ChangedButton == MouseButton.Left) this.DragMove();
 		}
 
 		private void SubmenuButton_Click(object sender, RoutedEventArgs e)
 		{
 			if ((sender as SubmenuButton)?.Submenu is Submenu submenu)
-			{
 				ViewModel.SelectedSubmenu = submenu;
-			}
 		}
 
 		private void ActionButton_Click(object sender, RoutedEventArgs e)
 		{
 			if ((sender as ActionButton)?.Action is ActionElement action)
-			{
 				ViewModel.SelectedAction = action;
-			}
 		}
 
 		private void Stepper_Scroll(int direction)
 		{
 			var values = (StepMode[])Enum.GetValues(typeof(StepMode));
 			int count = values.Length;
-
 			int currentIndex = (int)ViewModel.StepSize;
-
-			if (direction > 0)
-			{
-				currentIndex = (currentIndex - 1 + count) % count;
-			}
-			else
-			{
-				currentIndex = (currentIndex + 1) % count;
-			}
-
+			currentIndex = direction > 0 ? (currentIndex - 1 + count) % count : (currentIndex + 1) % count;
 			ViewModel.StepSize = (StepMode)currentIndex;
 		}
 
-		private void Stepper_LeftRight(int direction)
-		{
-			Stepper_Scroll(-1 * direction);
-		}
+		private void Stepper_LeftRight(int direction) => Stepper_Scroll(-1 * direction);
 
 		private void Menu_Scroll(int direction)
 		{
 			int count = ViewModel.UserMenus.Count;
 			int currentIndex = ViewModel.SelectedMenu != null ? ViewModel.UserMenus.IndexOf(ViewModel.SelectedMenu) : -1;
-
-			if (direction > 0)
-			{
-				currentIndex = (currentIndex - 1 + count) % count;
-			}
-			else
-			{
-				currentIndex = (currentIndex + 1) % count;
-			}
+			currentIndex = direction > 0 ? (currentIndex - 1 + count) % count : (currentIndex + 1) % count;
 			ViewModel.SelectedMenu = ViewModel.UserMenus[currentIndex];
 		}
 
 		private void Submenu_Scroll(int direction)
 		{
 			int count = ViewModel.SelectedMenu?.Submenus.Count ?? 0;
-
-			if (count == 0 || ViewModel.SelectedMenu == null)
-			{
-				return;
-			}
-
+			if (count == 0 || ViewModel.SelectedMenu == null) return;
 			int currentIndex = ViewModel.SelectedSubmenu != null ? ViewModel.SelectedMenu.Submenus.IndexOf(ViewModel.SelectedSubmenu) : -1;
-
-			if (direction > 0)
-			{
-				currentIndex = (currentIndex - 1 + count) % count;
-			}
-			else
-			{
-				currentIndex = (currentIndex + 1) % count;
-			}
-
+			currentIndex = direction > 0 ? (currentIndex - 1 + count) % count : (currentIndex + 1) % count;
 			ViewModel.SelectedSubmenu = ViewModel.SelectedMenu.Submenus[currentIndex];
 		}
 
-		private void Menu_LeftRight(int direction)
-		{
-			Menu_Scroll(-1 * direction);
-		}
-
-		private void Submenu_LeftRight(int direction)
-		{
-			Submenu_Scroll(-1 * direction);
-		}
-
-		private void Command_Scroll(int direction)
-		{
-			if (!ViewModel.IsConnected || ViewModel.SelectedAction == null)
-			{
-				return;
-			}
-
-			string[] steps = new string[] { "1%", "2%", "5%" };
-
-			var stepPercentage = steps[(int)ViewModel.StepSize];
-
-			Opacity = 0.01;
-
-			if (direction > 0)
-			{
-				Connector.Instance.SendCommandAsync(ViewModel.SelectedAction.Command + "=+" + stepPercentage);
-			}
-			else
-			{
-				Connector.Instance.SendCommandAsync(ViewModel.SelectedAction.Command + "=-" + stepPercentage);
-			}
-
-			_hideTimer.Stop();
-			_hideTimer.Start();
-		}
+		private void Menu_LeftRight(int direction) => Menu_Scroll(-1 * direction);
+		private void Submenu_LeftRight(int direction) => Submenu_Scroll(-1 * direction);
 
 		private void Command_UpDown(int direction)
 		{
-			if (ViewModel.SelectedSubmenu == null)
-			{
-				return;
-			}
-
+			if (ViewModel.SelectedSubmenu == null) return;
 			var actionItems = ViewModel.SelectedSubmenu.Items.OfType<ActionElement>().ToList();
 			int count = actionItems?.Count ?? 0;
-
-			if (count == 0 || actionItems == null)
-			{
-				return;
-			}
-
+			if (count == 0) return;
 			int currentIndex = ViewModel.SelectedAction != null ? actionItems.IndexOf(ViewModel.SelectedAction) : -1;
-
-			if (direction > 0)
-			{
-				currentIndex = (currentIndex - 1 + count) % count;
-			}
-			else
-			{
-				currentIndex = (currentIndex + 1) % count;
-			}
-
+			currentIndex = direction > 0 ? (currentIndex - 1 + count) % count : (currentIndex + 1) % count;
 			ViewModel.SelectedAction = actionItems[currentIndex];
 		}
 
-		private void Command_Reset()
-		{
-			Connector.Instance.SendCommandAsync(ViewModel.SelectedAction.Command + "=reset");
-		}
-
-		private void Step_Reset()
-		{
-			ViewModel.StepSize = 0;
-		}
-		private void Menu_Reset()
-		{
-			ViewModel.SelectedMenu = ViewModel.UserMenus.FirstOrDefault();
-		}
-
-		private void Submenu_Reset()
-		{
-			ViewModel.SelectedSubmenu = ViewModel.SelectedMenu?.Submenus.FirstOrDefault();
-		}
-
-		private void OnHideElapsed(object sender, ElapsedEventArgs e)
-		{
-			Dispatcher.Invoke(() =>
-			{
-				Opacity = 1;
-			});
-		}
+		private void Command_Reset() => Connector.Instance.SendCommandAsync(ViewModel.SelectedAction.Command + "=reset");
+		private void Step_Reset() => ViewModel.StepSize = 0;
+		private void Menu_Reset() => ViewModel.SelectedMenu = ViewModel.UserMenus.FirstOrDefault();
+		private void Submenu_Reset() => ViewModel.SelectedSubmenu = ViewModel.SelectedMenu?.Submenus.FirstOrDefault();
 	}
 }
